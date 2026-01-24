6ba6103e1deb28043584de29031d211f3800dab7
toretatenee <toretatenee@gmail.com>
2026-01-24 16:25:16 +0900

    MovieAiPropertyPageのWPF移植

--- views/MovieAiPropertyPage.xaml.cs
+++ views/MovieAiPropertyPage.xaml.cs
@@ -1,12 +1,12 @@
 using System;
-using System.Drawing;
 using System.Linq;
-using System.Windows.Forms;
+using System.Windows;
+using System.Windows.Controls;
 using DesktopAiMascot.aiservice;
 
-namespace DesktopAiMascot.Views
+namespace DesktopAiMascot.views
 {
-    public partial class MovieAiPropertyPage : UserControl
+    public partial class MovieAiPropertyPage : System.Windows.Controls.UserControl
     {
         public MovieAiPropertyPage()
         {
@@ -14,14 +14,13 @@ namespace DesktopAiMascot.Views
             PopulateMovieAiCombo();
         }
 
-        private void movieAiComboBox_SelectedIndexChanged(object? sender, EventArgs e)
+        private void MovieAiComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             if (movieAiComboBox.SelectedValue is string aiName)
             {
                 SystemConfig.Instance.MovieService = aiName;
                 SystemConfig.Instance.Save();
 
-                // MovieAiManager に即時反映
                 if (MovieAiManager.Instance.MovieAiServices.TryGetValue(aiName, out var service))
                 {
                     MovieAiManager.Instance.CurrentService = service;
@@ -29,31 +28,23 @@ namespace DesktopAiMascot.Views
             }
         }
 
-        // Movie AIエンジンコンボボックスの初期化
         private void PopulateMovieAiCombo()
         {
             try
             {
-                // イベントハンドラを一時的に解除
-                movieAiComboBox.SelectedIndexChanged -= movieAiComboBox_SelectedIndexChanged;
+                movieAiComboBox.SelectionChanged -= MovieAiComboBox_SelectionChanged;
 
-                // バインディング設定
-                movieAiComboBox.DataSource = MovieAiManager.Instance.MovieAiServices.Values.ToList();
-                movieAiComboBox.DisplayMember = "Name";
-                movieAiComboBox.ValueMember = "Name";
+                movieAiComboBox.ItemsSource = MovieAiManager.Instance.MovieAiServices.Values.ToList();
 
-                // 現在の設定を選択
                 string currentService = SystemConfig.Instance.MovieService;
                 movieAiComboBox.SelectedValue = currentService;
 
-                // もし選択できていなければデフォルト(0番目)を選択
                 if (movieAiComboBox.SelectedIndex < 0 && movieAiComboBox.Items.Count > 0)
                 {
                     movieAiComboBox.SelectedIndex = 0;
                 }
 
-                // イベントハンドラを再設定
-                movieAiComboBox.SelectedIndexChanged += movieAiComboBox_SelectedIndexChanged;
+                movieAiComboBox.SelectionChanged += MovieAiComboBox_SelectionChanged;
             }
             catch (Exception ex)
             {
