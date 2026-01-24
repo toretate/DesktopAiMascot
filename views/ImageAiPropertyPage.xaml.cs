b75a3eff8c8c631190ac4131bed0320f535ea540
toretatenee <toretatenee@gmail.com>
2026-01-24 16:24:41 +0900

    ImageAiPropertyPageのWPF移植

--- views/ImageAiPropertyPage.xaml.cs
+++ views/ImageAiPropertyPage.xaml.cs
@@ -1,12 +1,12 @@
 using System;
-using System.Drawing;
 using System.Linq;
-using System.Windows.Forms;
+using System.Windows;
+using System.Windows.Controls;
 using DesktopAiMascot.aiservice;
 
-namespace DesktopAiMascot.Views
+namespace DesktopAiMascot.views
 {
-    public partial class ImageAiPropertyPage : UserControl
+    public partial class ImageAiPropertyPage : System.Windows.Controls.UserControl
     {
         public ImageAiPropertyPage()
         {
@@ -14,14 +14,13 @@ namespace DesktopAiMascot.Views
             PopulateImageAiCombo();
         }
 
-        private void imageAiComboBox_SelectedIndexChanged(object? sender, EventArgs e)
+        private void ImageAiComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             if (imageAiComboBox.SelectedValue is string aiName)
             {
                 SystemConfig.Instance.ImageService = aiName;
                 SystemConfig.Instance.Save();
 
-                // ImageAiManager に即時反映
                 if (ImageAiManager.Instance.ImageAiServices.TryGetValue(aiName, out var service))
                 {
                     ImageAiManager.Instance.CurrentService = service;
@@ -29,31 +28,23 @@ namespace DesktopAiMascot.Views
             }
         }
 
-        // Image AIエンジンコンボボックスの初期化
         private void PopulateImageAiCombo()
         {
             try
             {
-                // イベントハンドラを一時的に解除
-                imageAiComboBox.SelectedIndexChanged -= imageAiComboBox_SelectedIndexChanged;
+                imageAiComboBox.SelectionChanged -= ImageAiComboBox_SelectionChanged;
 
-                // バインディング設定
-                imageAiComboBox.DataSource = ImageAiManager.Instance.ImageAiServices.Values.ToList();
-                imageAiComboBox.DisplayMember = "Name";
-                imageAiComboBox.ValueMember = "Name";
+                imageAiComboBox.ItemsSource = ImageAiManager.Instance.ImageAiServices.Values.ToList();
 
-                // 現在の設定を選択
                 string currentService = SystemConfig.Instance.ImageService;
                 imageAiComboBox.SelectedValue = currentService;
 
-                // もし選択できていなければデフォルト(0番目)を選択
                 if (imageAiComboBox.SelectedIndex < 0 && imageAiComboBox.Items.Count > 0)
                 {
                     imageAiComboBox.SelectedIndex = 0;
                 }
 
-                // イベントハンドラを再設定
-                imageAiComboBox.SelectedIndexChanged += imageAiComboBox_SelectedIndexChanged;
+                imageAiComboBox.SelectionChanged += ImageAiComboBox_SelectionChanged;
             }
             catch (Exception ex)
             {
