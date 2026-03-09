140879cd1f60505d3b69f0f69cba960561329091
toretatenee <toretatenee@gmail.com>
2026-03-09 10:49:44 +0900

    refactor(ui): 設定画面関連をUIコード生成から.tscnシーンベースに移行

--- ui/settings/pages/ChatAiPropertyPage.cs
+++ ui/settings/pages/ChatAiPropertyPage.cs
@@ -4,10 +4,5 @@ namespace DesktopAiMascot.ui.settings.pages
 {
     public partial class ChatAiPropertyPage : MarginContainer
     {
-        public ChatAiPropertyPage()
-        {
-            SetAnchorsPreset(Godot.Control.LayoutPreset.FullRect);
-            AddChild(new Godot.Label { Text = "Chat AI Settings (WIP)", HorizontalAlignment = Godot.HorizontalAlignment.Center });
-        }
     }
 }
--- ui/settings/pages/ImageAiPropertyPage.cs
+++ ui/settings/pages/ImageAiPropertyPage.cs
@@ -4,10 +4,5 @@ namespace DesktopAiMascot.ui.settings.pages
 {
     public partial class ImageAiPropertyPage : MarginContainer
     {
-        public ImageAiPropertyPage()
-        {
-            SetAnchorsPreset(Godot.Control.LayoutPreset.FullRect);
-            AddChild(new Godot.Label { Text = "Image AI Settings (WIP)", HorizontalAlignment = Godot.HorizontalAlignment.Center });
-        }
     }
 }
